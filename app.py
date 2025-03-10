26390e6b7919deff73f4586d08a3af0e2fe2e312
Achraf <achrafhafid565@gmail.com>
2025-03-10 22:39:49 +0100

    fix frontend url

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 from kpi_processor import KPIProcessor
 
 app = Flask(__name__)
-CORS(app, origins=["http://localhost:3000/kpi-automatisation-frontend-react" ])
+CORS(app, origins=["https://achrafh17.github.io/kpi-automatisation-frontend-react/" ])
 
 
 UPLOAD_FOLDER = 'uploads'
